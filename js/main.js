983c17399b3fe856288b3eb47cf184f5c9388efc
Eliswer <106431426+Eliswer@users.noreply.github.com>
2022-07-07 19:17:34 +0200

    tasks

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//1
+let fruitsArr = [];
+fruitsArr = ['apple', 'banana', 'orange', 'pomegranade', 'pear'];
+const arrPush = fruitsArr.push('melon');
+
+fruitsArr.forEach(function(printValues, index, arr){
+    console.log(printValues);
+})
+
+//2
+for (let input = 1; input <= 10; input++) {
+    console.log(input);
+}
+
+for (let input = 10; input >= 1; input--) {
+    console.log(input);
+}
