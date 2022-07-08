ee884e83460978c9cb1f4ca26943d050ad14f4ac
Eliswer <106431426+Eliswer@users.noreply.github.com>
2022-07-08 14:14:16 +0200

    started a new challenge

--- js/main.js
+++ js/main.js
@@ -26,15 +26,30 @@ const heightMark = 1.69;
 const heightJohn = 1.95;
 const weightJohn = 92;
 
-let BmiMark = (weightMark / heightMark **2);
-let BmiJohn = (weightJohn / heightJohn **2);
-console.log(BmiMark, BmiJohn);
+let bmiMark = (weightMark / heightMark **2);
+let bmiJohn = (weightJohn / heightJohn **2);
+console.log(bmiMark, bmiJohn);
 
-const markHigherBMI = BmiMark > BmiJohn;
+const markHigherBMI = bmiMark > bmiJohn;
 console.log(markHigherBMI);
 
-//Georgis task
+//Georgi`s task
 
 let userHeight = parseFloat(prompt('Whats your height in cm?'));
 let userWeight = parseFloat(prompt('Whats your weight in kg ?'));
 console.log(userWeight / userHeight ** 2);
+
+
+//Coding challenge #2
+
+// Use the BMI example from Challenge #1, and the code you already wrote, and
+// improve it.
+//     Your tasks:
+//     1. Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+
+if (bmiJohn > bmiMark) {
+    console.log(`John's BMI is higher than Mark's!"`)
+}
