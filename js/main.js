5962821ec9b5eeceed0f5f057d22dae70537abe0
Eliswer <106431426+Eliswer@users.noreply.github.com>
2022-07-21 16:24:10 +0200

    learned how functions work

--- js/main.js
+++ js/main.js
@@ -26,3 +26,51 @@ console.log(fruitProcessor(5,0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+//// function declaration
+
+function calcAge1 (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+//// function expression
+////anonymous function - expression produces value
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+} //this variable holds this functions value, functions are value, they produce value
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+////Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+////Functions calling other functions
+
+function cutPieces(fruit2) {
+    return fruit2 * 4;
+}
+
+function fruitProcessor2(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
