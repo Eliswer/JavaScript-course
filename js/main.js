6578dfdc45b8356b05ff40b8c4c1a141cf9fa423
Eliswer <106431426+Eliswer@users.noreply.github.com>
2022-07-29 13:59:59 +0200

    learned while loop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Loops
+// Loops allow programs to perform repetitive tasks, such as iterating through an array
 
-//for loop, it has a counter
+// For loop, it has a counter
 
 // console.log('Lifting weights repetition 1')
 // console.log('Lifting weights repetition 2')
@@ -15,9 +16,9 @@
 
 //creating a value and telling it where to start
 //rep++ is the same as rep = rep + 1;
-for (let rep = 1; rep <=10; rep++) { //as soon as this condition is false the loop stops = rep <=10 aka for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) { //as soon as this condition is false the loop stops = rep <=10 aka for loop keeps running while condition is TRUE
     console.log(`Lifting weights repetition ${rep}`)
-}
+};
 
 const eliArray = [
     'Eli',
@@ -28,6 +29,8 @@ const eliArray = [
     true
 ];
 
+const types = []; //empty array
+
 // console.log(eliArray[0]);
 // console.log(eliArray[1]);
 // --------
@@ -35,5 +38,92 @@ const eliArray = [
 //eliArray[5] does not exist
 
 for (let i = 0; i < eliArray.length; i++ ) {
+    //reading from eliArray array
+    console.log(eliArray[i], typeof eliArray[i]);
+
+    //filling types array
+    types[i] = typeof eliArray[i];
+    types.push(typeof eliArray[i]);
+}
+
+console.log(types);
+
+////
+
+const years = [
+    1991,
+    2007,
+    1969,
+    2020
+];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2022 - years[i]); //2022 - 1991, then 2022 - 2007 and so on, thanks to the years[i] = stored in ages
+}
+console.log(ages);
+
+// continue and break
+// The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+// The break statement ends the loop ("breaks" the loop) when there is a met condition
+console.log('---- ONLY STRINGS ----');
+for (let i = 0; i < eliArray.length; i++ ) {
+    if (typeof eliArray[i] !== 'string') continue;
+
     console.log(eliArray[i], typeof eliArray[i]);
 }
+
+console.log('---- BREAK WITH NUMBER ----');
+for (let i = 0; i < eliArray.length; i++ ) {
+    if (typeof eliArray[i] == 'number') break;
+
+    console.log(eliArray[i], typeof eliArray[i]);
+}
+
+////
+console.log('---- LOOPING BACKWARDS ----')
+
+const eli = [
+    'Eli',
+    'Bond',
+    2022 - 2003,
+    'programmer',
+    ['Denisa', 'Ivana', 'Clare'],
+    true
+];
+
+//looping through an array backwards
+for (let i = eli.length - 1; i >= 0; i--) {
+    console.log(i, eli[i]);
+}
+
+////
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`--- Starting exercise ${exercise}`);
+
+    for (let rep2 = 1; rep2 < 6; rep2++) {
+        console.log(`Exercise ${exercise} lifting weight repetition ${rep2}`)
+    }
+}
+
+// While loop
+
+for (let rep3 = 1; rep3 <= 10; rep3++) {
+    console.log(`FOR: Lifting weights repetition ${rep3}`)
+};
+
+
+let rep = 1;
+while (rep <= 10) { //it will run while this condition is true
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++
+};
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end !')
+};
